b90b91dcf55e1b62cbccbe4852803a1b0db89e74
Jason Hartgraves <jharttech@hotmail.com>
2022-11-30 10:34:12 -0600

    Corrected path name.

--- helper_tools/csv_compose.py
+++ helper_tools/csv_compose.py
@@ -1,10 +1,12 @@
+import csv
+
 # The Compose class simply composes a file that can then be moved or reused for further
 # Data manipulation
 class Compose:
     def __init__(self, staged_data):
         self.o_filename = staged_data[1]
         self.lines = staged_data[0]
-        with open(f"needed_file/{self.o_filename}", mode="w") as self.csv_file:
+        with open(f"needed_files/{self.o_filename}", mode="w") as self.csv_file:
             for i in range(0, len(self.lines)):
                 self.full = csv.writer(self.csv_file, delimiter=",")
                 self.full.writerow(self.lines[i])
