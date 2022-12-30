0906a4b65156712ac6bf1e435c38ea683d5f6a55
Jason Hartgraves <jharttech@hotmail.com>
2022-12-30 10:31:04 -0600

    Added the Dict_Print class.

--- helper_tools/misc.py
+++ helper_tools/misc.py
@@ -10,3 +10,13 @@ class Compose:
             for i in range(0, len(self.lines)):
                 self.full = csv.writer(self.csv_file, delimiter=",")
                 self.full.writerow(self.lines[i])
+
+
+class Dict_Print:
+    def __init__(self,data):
+        self.data = data
+        self.data_list = list(map(int,self.data))
+        self.data_list = sorted(self.data_list)
+        print("\n")
+        for i in range(0,len(self.data)):
+            print(str(self.data_list[i]) +  " : " + self.data.get(str(self.data_list[i])))
