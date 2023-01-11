819b67065728ec13dc0814cb838e0e6bb11614e7
Jason Hartgraves <jharttech@hotmail.com>
2023-01-11 09:00:14 -0600

    Cleaned up verbage of current OU result.

--- user_account_tools/move_user.py
+++ user_account_tools/move_user.py
@@ -7,7 +7,7 @@ def move_user():
     user_account = input("What user would you like to move? (Only username needed) ")
     user_info = subprocess.Popen(["gam", "info", "user", user_account], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     user_current_ou = subprocess.Popen(["grep", "Google Org Unit Path"], stdin=user_info.stdout, stdout=subprocess.PIPE)
-    result = str(user_current_ou.stdout.read().decode().strip())
+    result = ("Users current " + str(user_current_ou.stdout.read().decode().strip()))
     return result
 
 
