65616887c5dd7995327b1aced8861548dd757602
Jason Hartgraves <jharttech@hotmail.com>
2023-01-11 08:58:38 -0600

    Corrected syntax error in stderr of subprocess.

--- user_account_tools/move_user.py
+++ user_account_tools/move_user.py
@@ -4,8 +4,8 @@ import csv
 
 
 def move_user():
-    user_account = input("What user would you like to move? (Only username needed)")
-    user_info = subprocess.Popen(["gam", "info", "user", user_account], stdout=subprocess.PIPE, strerr=subprocess.PIPE)
+    user_account = input("What user would you like to move? (Only username needed) ")
+    user_info = subprocess.Popen(["gam", "info", "user", user_account], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     user_current_ou = subprocess.Popen(["grep", "Google Org Unit Path"], stdin=user_info.stdout, stdout=subprocess.PIPE)
     result = str(user_current_ou.stdout.read().decode().strip())
     return result
