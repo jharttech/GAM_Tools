98b8ce37690488bc8271f521f1a972fe1c8bb2a1
Jason Hartgraves <jharttech@hotmail.com>
2023-01-11 08:46:22 -0600

    Initial upload and start on move_user tool.

---
+++ user_account_tools/move_user.py
@@ -0,0 +1,19 @@
+import sys
+import subprocess
+import csv
+
+
+def move_user():
+    user_account = input("What user would you like to move? (Only username needed)")
+    user_info = subprocess.Popen(["gam", "info", "user", user_account], stdout=subprocess.PIPE, strerr=subprocess.PIPE)
+    user_current_ou = subprocess.Popen(["grep", "Google Org Unit Path"], stdin=user_info.stdout, stdout=subprocess.PIPE)
+    result = str(user_current_ou.stdout.read().decode().strip())
+    return result
+
+
+def main():
+    print(move_user())
+
+
+if __name__ == "__main__":
+    main()
