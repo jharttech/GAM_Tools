b6c25908333a75412a707041ee150b3a95f91f36
Jason Hartgraves <jharttech@hotmail.com>
2023-01-20 13:45:24 -0600

    Adding comment and cleaning up code.

--- helper_tools/device_data.py
+++ helper_tools/device_data.py
@@ -1,38 +1,27 @@
-import csv
 import subprocess
-import datetime
 
-
+# Define Full_Device_Data Class
 class Full_Device_Data:
     def __init__(self):
+        # Set the needed path and file
         self.filename = "needed_files/full_list_devices.csv"
-        subprocess.Popen(["touch",self.filename], stdout=subprocess.PIPE)
+        # Create the file
+        subprocess.Popen(["touch", self.filename], stdout=subprocess.PIPE)
 
         self.write_data(self.filename)
 
-    def write_data(self,filename):
+    def write_data(self, filename):
         print("Now going to collect all device data from GAM")
         self.filename = filename
-        with open(self.filename, mode='w') as self.o_file:
-            self.write_out = subprocess.Popen(["gam","print","cros","full","query","status:provisioned"], stdout=self.o_file)
+        # Open the file for writing
+        with open(self.filename, mode="w") as self.o_file:
+            # Write all device info into open file
+            self.write_out = subprocess.Popen(
+                ["gam", "print", "cros", "full", "query", "status:provisioned"],
+                stdout=self.o_file,
+            )
             self.write_out.wait()
 
-class Wanted_Devices_Data:
-    def __init__(self):
-        self.header_list = [
-            'deviceId',
-            'autoUpdateExpiration',
-            'serialNumber',
-            'macAddress',
-            'model',
-            'orgUnitPath',
-            #REMAINING SPACE ON DEVICE
-        ]
-        self.header_to_num = {}
-        self.lines = []
-        self.temp_row = []
-        self.num = None
-                
 
 def main():
     Full_Device_Data()
