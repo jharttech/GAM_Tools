15c44865ee0992d27a3882614bd3ee4fe1ba033d
Jason Hartgraves <jharttech@hotmail.com>
2023-01-20 14:12:24 -0600

    Adding comments and cleaning up code.

--- helper_tools/user_data.py
+++ helper_tools/user_data.py
@@ -6,16 +6,17 @@ import user_account_tools.create_account
 from helper_tools import misc
 
 
-
 # The Account_type class simply asks the user whether the user wants to use
 # Staff data or student data and returns the user input as a string.
 class Account_type:
     def __init__(self, account):
+        # Define dictionary of types of accounts we use on campus
         self.dict = {"1": "student", "2": "staff", "3": "exit"}
-
         self.a_type = self.dict.get(account)
+
         if self.a_type == "exit":
-            sys.exit("You have chosen to exit.")
+            print("You have chosen to exit.")
+            misc.exit_message()
 
     def __str__(self):
         return self.a_type
@@ -33,20 +34,22 @@ class Account_type:
                 return cls(account)
 
 
+# Define Get_User_Data class to gather the wanted user data from the desired org unit
 class Get_User_Data:
-    def __init__(self, account_type, org_units,selected_ou):
+    def __init__(self, account_type, org_units, selected_ou):
         self.account_type = account_type
         self.org_units = org_units
         self.selected_ou = selected_ou
         self.selected_ou_name = self.selected_ou.split("/")
-        self.selected_ou_name = self.selected_ou_name[len(self.selected_ou_name)-1]
+        self.selected_ou_name = self.selected_ou_name[len(self.selected_ou_name) - 1]
         self.gather_data(self.selected_ou)
 
-    def gather_data(self,selected_ou):
+    # Define gather_data method
+    def gather_data(self, selected_ou):
         if str(self.account_type) == "student":
-            with open(
-                "needed_files/list_student_data.csv", mode="w"
-            ) as needed_file:
+            # Open file for writing
+            with open("needed_files/list_student_data.csv", mode="w") as needed_file:
+                # Run GAM command to get desired user data from desired org unit
                 gather = subprocess.Popen(
                     [
                         "gam",
@@ -58,9 +61,12 @@ class Get_User_Data:
                     ],
                     stdout=needed_file,
                 )
+                # Wait for the subprocess to finish as it could take some time
                 gather.wait()
         elif str(self.account_type) == "staff":
+            # Open file for writing
             with open("needed_files/list_staff_data.csv", mode="w") as needed_file:
+                # Run GAM command to get deisred user data from desired org unit
                 gather = subprocess.Popen(
                     [
                         "gam",
@@ -72,21 +78,28 @@ class Get_User_Data:
                     ],
                     stdout=needed_file,
                 )
+                # wait for subprocess to finish as it could take some time
                 gather.wait()
 
     def __str__(self):
         return self.selected_ou_name
 
     @classmethod
-    def get(cls,account_type,org_units):
+    def get(cls, account_type, org_units):
         while True:
             wanted_ou = input("Please enter which Org Unit you want user data from: ")
+            # Validate user input and if invalid ask again
             if str(wanted_ou) not in org_units:
-                print("Invalid entry, please try again! (Enter 1-" + str(len(org_units)) + ")")
+                print(
+                    "Invalid entry, please try again! (Enter 1-"
+                    + str(len(org_units))
+                    + ")"
+                )
             else:
+                # Convert user option number to the actuall org unit name
                 selected_ou = str(org_units.get(str(wanted_ou)))
                 break
-        return cls(account_type,org_units,selected_ou)
+        return cls(account_type, org_units, selected_ou)
 
 
 # The Stage_csv class ultimately returns a list of values for
@@ -94,7 +107,7 @@ class Get_User_Data:
 # Data that is being worked with
 class Stage_csv:
     # Set inital class variables
-    def __init__(self, account_type,selected_ou):
+    def __init__(self, account_type, selected_ou):
         self.g_headers = [
             "primaryEmail",
             "name.givenName",
@@ -123,6 +136,7 @@ class Stage_csv:
     def stage(
         self,
     ):
+        # Open file for reading
         with open("needed_files/" + self.i_filename, mode="r") as self.csv_file:
             self.csv_reader = csv.reader(self.csv_file, delimiter=",")
             self.n_col = len(next(self.csv_reader))
@@ -218,11 +232,11 @@ class Compose:
     def __init__(self, staged_data):
         self.o_filename = staged_data[1]
         self.lines = staged_data[0]
+        # Open file for writing
         with open("needed_files/" + self.o_filename, mode="w") as self.csv_file:
             for i in range(0, len(self.lines)):
                 self.full = csv.writer(self.csv_file, delimiter=",")
                 self.full.writerow(self.lines[i])
-
 
 
 # The move_file function moves the created files to where they need to go
@@ -259,8 +273,8 @@ def main():
     account_type = Account_type.get()
     campus_OUs = user_account_tools.create_account.Campus_OUs().ou_dict(account_type)
     misc.Dict_Print(campus_OUs)
-    selected_ou = Get_User_Data.get(account_type,campus_OUs)
-    staged = Stage_csv(account_type,selected_ou).stage()
+    selected_ou = Get_User_Data.get(account_type, campus_OUs)
+    staged = Stage_csv(account_type, selected_ou).stage()
     Compose(staged)
     move_file(staged)
 
