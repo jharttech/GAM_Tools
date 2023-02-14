fd93f02c8b2b18ffa5560d9dabf8beb5404a5aaa
Jason Hartgraves <jharttech@hotmail.com>
2023-02-14 11:50:59 -0600

    Debugging.

--- device_tools/find_device_data_by_org_unit.py
+++ device_tools/find_device_data_by_org_unit.py
@@ -3,8 +3,29 @@ from helper_tools import (device_data, misc)
 
 account_type = "Chromebooks"
 
+class Choosen_OU:
+    def __init__(self,ou):
+        self.ou = ou
+    
+    def __str__(self):
+        return self.ou
+
+    @classmethod
+    def get(cls, ou_dict):
+        while True:
+            choice = input("\nPlease select the desired Org Unit: ")
+            if str(choice) not in ou_dict:
+                print("Invalid entry, please try again! (Enter 1-" + str(len(ou_dict)) + ")")
+            else:
+                ou = ou_dict.get(choice)
+                return cls(ou)
+
+
 class Stage_CSV:
     def __init__(self,device_org_unit):
+        self.ou = device_org_unit.split("/")
+        self.ou = self.ou[len(self.ou)-1]
+        print(self.ou)
         self.i_filename = "needed_files/full_list_devices.csv"
         self.o_filename = "cart_device_data/" + device_org_unit
         self.g_headers = [
@@ -29,6 +50,8 @@ class Stage_CSV:
 def main():
     device_OUs = misc.Campus_OUs().ou_dict(account_type)
     misc.Dict_Print(device_OUs)
+    choosen_ou = Choosen_OU().get(device_OUs)
+    Stage_CSV(choosen_ou)
 
 
 
