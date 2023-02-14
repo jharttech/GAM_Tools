47df1ee7daf0d7c6072f19572328e8ea2a6c3046
Jason Hartgraves <jharttech@hotmail.com>
2023-02-14 11:07:59 -0600

    Moved Campus_OUs to misc.

--- helper_tools/misc.py
+++ helper_tools/misc.py
@@ -54,6 +54,57 @@ class Setup:
             data_file = subprocess.Popen(["gam", "print", "orgs"], stdout=write_file)
             data_file.wait()
 
+# The Campus_OUs class gathers the campus OU's and puts them into a dictionary with numeric keys
+class Campus_OUs:
+    # We do not need the init method for this class so we move on
+    def __init__(self):
+        ...
+
+    # The ou_dict method takes only the relevant Org Units for the desired account type and
+    # Assigns them to a dictionary with numeric keys
+    def ou_dict(self, account_type):
+        self.account_type = account_type
+
+        if str(self.account_type) == "staff":
+            self.ou_type = "Employees"
+        else:
+            self.ou_type = "Students"
+
+        self.org_unit_dict = {}
+
+        with open("needed_files/org_units", mode="r") as self.csv_file_read:
+            self.read_file = csv.reader(self.csv_file_read, delimiter=",")
+            # Read the number of columns
+            self.n_col = len(next(self.read_file))
+            # Return the reader back to start of file
+            self.csv_file_read.seek(0)
+            # Keep track of line
+            self.line_count = 0
+            # Keep track of numeric value of the key
+            self.dict_key_count = 0
+            for row in self.read_file:
+                if self.line_count == 0:
+                    # Look through columns
+                    for x in range(0, self.n_col):
+                        # Set the column name to the header value of each column
+                        self.col_name = str(row[x])
+                        # Find the number of the column that has the header name of orgUnitPath
+                        if str(self.col_name) == "orgUnitPath":
+                            self.num = x
+                            # Increase line count
+                            self.line_count += 1
+                else:
+                    self.line_count += 1
+                    if str(self.ou_type) in row[self.num]:
+                        # Increase the key count so the numeric values start at 1 and not 0
+                        self.dict_key_count += 1
+                        # Add the numberic key and the value to the dictionary of Org Units
+                        self.org_unit_dict.update(
+                            {str(self.dict_key_count): row[self.num]}
+                        )
+
+        return self.org_unit_dict
+
 
 # Defind the exit_message function to be called any time the program is asked or wants to close.
 def exit_message():
--- user_account_tools/create_account.py
+++ user_account_tools/create_account.py
@@ -5,58 +5,6 @@ import datetime
 from helper_tools import user_data, misc
 
 
-# The Campus_OUs class gathers the campus OU's and puts them into a dictionary with numeric keys
-class Campus_OUs:
-    # We do not need the init method for this class so we move on
-    def __init__(self):
-        ...
-
-    # The ou_dict method takes only the relevant Org Units for the desired account type and
-    # Assigns them to a dictionary with numeric keys
-    def ou_dict(self, account_type):
-        self.account_type = account_type
-
-        if str(self.account_type) == "staff":
-            self.ou_type = "Employees"
-        else:
-            self.ou_type = "Students"
-
-        self.org_unit_dict = {}
-
-        with open("needed_files/org_units", mode="r") as self.csv_file_read:
-            self.read_file = csv.reader(self.csv_file_read, delimiter=",")
-            # Read the number of columns
-            self.n_col = len(next(self.read_file))
-            # Return the reader back to start of file
-            self.csv_file_read.seek(0)
-            # Keep track of line
-            self.line_count = 0
-            # Keep track of numeric value of the key
-            self.dict_key_count = 0
-            for row in self.read_file:
-                if self.line_count == 0:
-                    # Look through columns
-                    for x in range(0, self.n_col):
-                        # Set the column name to the header value of each column
-                        self.col_name = str(row[x])
-                        # Find the number of the column that has the header name of orgUnitPath
-                        if str(self.col_name) == "orgUnitPath":
-                            self.num = x
-                            # Increase line count
-                            self.line_count += 1
-                else:
-                    self.line_count += 1
-                    if str(self.ou_type) in row[self.num]:
-                        # Increase the key count so the numeric values start at 1 and not 0
-                        self.dict_key_count += 1
-                        # Add the numberic key and the value to the dictionary of Org Units
-                        self.org_unit_dict.update(
-                            {str(self.dict_key_count): row[self.num]}
-                        )
-
-        return self.org_unit_dict
-
-
 # The Assign_OU class askes the user which Org Unit they want to assign the new user(s) to.
 class Assign_OU:
     def __init__(self, ou):
@@ -326,7 +274,7 @@ def main():
     # Call the Account_type class from the user_script module
     account_type = user_data.Account_type.get()
     # Run the Campus_OUs class to get the OUs available in the campus and save the return to variable
-    campus_OUs = Campus_OUs().ou_dict(account_type)
+    campus_OUs = misc.Campus_OUs().ou_dict(account_type)
     # Call the dict_print function to print the desired dictionary
     misc.Dict_Print(campus_OUs)
     # Run the Assign_OU class and assign the return to variable
